c8fef735b9e008590dba43675f175d09c8bc74c7
mrcamjo12 <130695878+mrcamjo12@users.noreply.github.com>
2023-07-09 15:33:00 -0700

    backendlinks

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ displayFaceBox = (box) => {
 // Everything for onButtonSubmit is in final (do not change)
   onButtonSubmit = () => {
    this.setState({imageUrl: this.state.input})
-  fetch ('http://localhost:3000/imageurl', {
+  fetch ('https://bacend.onrender.com/imageurl', {
     method: 'post', 
     headers: {'Content-Type': 'application/json'},
     body: JSON.stringify({
@@ -79,7 +79,7 @@ displayFaceBox = (box) => {
  .then(response => response.json())
  .then(response => {
   if (response) {
-    fetch('http://localhost:3000/image', {
+    fetch('https://bacend.onrender.com:3000/image', {
       method: 'put', 
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
