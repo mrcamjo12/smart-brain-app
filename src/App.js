9ffd536b0ce3cebeeac5a9dab9de7972b5d41125
mrcamjo12 <130695878+mrcamjo12@users.noreply.github.com>
2023-07-09 16:31:08 -0700

    updatedbagain

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ displayFaceBox = (box) => {
  .then(response => response.json())
  .then(response => {
   if (response) {
-    fetch('https://bacend.onrender.com:3000/image', {
+    fetch('https://bacend.onrender.com/image', {
       method: 'put', 
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
